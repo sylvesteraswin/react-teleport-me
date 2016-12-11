2fa882b8ef7183597b3e18915c3f015b2e47cbb9
Sylvester Aswin <sylvester.aswinstanley@gmail.com>
2016-12-12 00:28:37 +0100

    Initial setup.

---
+++ src/app.jsx
@@ -0,0 +1,52 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+import { DelayRenderFactory } from '../../lib/Teleport';
+
+class App extends Component {
+    handleMouseMove = () => {
+        console.log(`mousemove ${this.iamhere} Hello`);
+    };
+
+    iamhere = 'Something is here';
+
+    render = () => {
+        return (
+            <section
+                className="app">
+                Hello There
+            </section>
+        );
+    }
+};
+
+const AppDelay = DelayRenderFactory()(App);
+
+class MyApp extends Component {
+    state = {
+        active: false
+    };
+    _toggleState = () => {
+        this.setState({
+            active: !this.state.active,
+        });
+    };
+
+    render = () => {
+        return (
+            <section
+                className="app">
+                <button
+                    className="f6 link dim br-pill ph3 pv2 mb2 dib white bg-black"
+                    onClick={this._toggleState}
+                    >
+                    Show {this.props.title}
+                </button>
+
+                <AppDelay active={this.state.active} />
+            </section>
+        );
+    }
+}
+
+ReactDOM.render(<MyApp title={"Hello"} />, document.getElementById('container'));
